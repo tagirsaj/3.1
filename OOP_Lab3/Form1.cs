2a7ac39919e75691cab54419f213ccb4e6815d4f
Tagir <boss.saydelov@mail.ru>
2026-04-19 21:04:18 +0500

    Создание хранилища для множества кругов

--- OOP_Lab3/Form1.cs
+++ OOP_Lab3/Form1.cs
@@ -1,33 +1,37 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
 namespace OOP_Lab3
 {
+    public class CCircle
+    {
+        public int X, Y;
+        public CCircle(int x, int y) { X = x; Y = y; }
+        public void Draw(Graphics g) => g.DrawEllipse(Pens.Black, X - 30, Y - 30, 60, 60);
+    }
+
     public partial class Form1 : Form
     {
-        // одна координата
-        private Point? circlePos = null;
+        private List<CCircle> storage = new List<CCircle>();
 
         public Form1()
         {
             InitializeComponent();
-            this.Text = "Этап 1: Рисуем один круг";
+            this.Text = "Лаба 3.1";
+            this.DoubleBuffered = true;
         }
 
         protected override void OnMouseDown(MouseEventArgs e)
         {
-            circlePos = e.Location;
+            storage.Add(new CCircle(e.X, e.Y));
             this.Invalidate();
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            if (circlePos.HasValue)
-            {
-                // Крууг
-                e.Graphics.DrawEllipse(Pens.Black, circlePos.Value.X - 30, circlePos.Value.Y - 30, 60, 60);
-            }
+            foreach (var circle in storage) circle.Draw(e.Graphics);
         }
     }
 }
